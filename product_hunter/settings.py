9dace51b33f8b9c9d14a7c184435998ad5f30e7e
Kaniska <racefuel@users.noreply.github.com>
2019-10-15 13:46:26 +0530

    Users can now add products

--- product_hunter/settings.py
+++ product_hunter/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "product_hunter/static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# P̶r̶o̶d̶u̶c̶t̶ c̶l̶a̶s̶s̶:̶
+# t̶i̶t̶l̶e̶, u̶r̶l̶, p̶u̶b̶_̶d̶a̶t̶e̶, t̶o̶t̶a̶l̶ v̶o̶t̶e̶s̶, i̶m̶a̶g̶e̶, i̶c̶o̶n̶, b̶o̶d̶y̶/̶d̶e̶s̶c̶r̶i̶p̶t̶i̶o̶n̶
+# p̶u̶b̶_̶d̶a̶t̶e̶_̶p̶r̶e̶t̶t̶y̶, hunter
+
+class Product(models.Model):
+
+    title = models.CharField(max_length = 250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+    icon = models.ImageField(upload_to = 'images/')
+    votes_total = models.IntegerField(default = 1)
+    hunter = models.ForeignKey(User, on_delete = models.CASCADE) # Products are also deleted if the user(referenced object) is deleted
+
+    def __str__(self):
+        return self.title
+    
+    def description(self):
+        return self.body[:100]
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
